b135a1d3dd871743f543ec9e3248dd3e10c4e5c6
Dean Selvey <deanselvey@gmail.com>
2018-12-16 19:57:49 +0000

    Created shell component and added routing

--- dynki/src/index.tsx
+++ dynki/src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+)
+
 ReactDOM.render(
-  <App />,
+  routing,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
